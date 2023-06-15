1efa7b0ab79393e94b82e3cbc4054aa09b93b4b3
dev-miguel-resources <ingenieromiguelch@gmail.com>
2023-06-14 20:52:34 -0400

    children props

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import Greetings from "./Greetings";
+import Greetings from './Greetings'
 
 function App() {
   return (
     <div>
       <h1>Welcome to React Props!</h1>
-      <Greetings 
-        firstName={"Paulina"}
-        lastName="Ávila"
-        age={30}
-      />
+      <Greetings firstName={'Paulina'} lastName="Ávila" age={30}>
+        <img src="https://picsum.photos/200/300" alt="picsum1" />
+        <img src="https://picsum.photos/200/300" alt="picsum2" />
+      </Greetings>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Greetings.jsx
+++ src/Greetings.jsx
@@ -3,13 +3,15 @@ import PropTypes from 'prop-types'
 // Tip Clean Code: Para recibir props una x una, ideal que sea hasta un máximo de 4 params
 const Greetings = (props) => {
   console.log(props)
-  const { firstName, lastName, age } = props;
+  const { firstName, lastName, age } = props
   // props.age = 40
   //const agePerson = age + 40
 
   return (
     <div>
       Hi {firstName} {lastName} you are {age}
+      <br />
+        {props.children}
     </div>
   )
 }
@@ -17,7 +19,8 @@ const Greetings = (props) => {
 export default Greetings
 
 Greetings.propTypes = {
-    firstName: PropTypes.string,
-    lastName: PropTypes.string,
-    age: PropTypes.number
+  firstName: PropTypes.string,
+  lastName: PropTypes.string,
+  age: PropTypes.number,
 }
+
